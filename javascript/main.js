084240616340d5790a3bea0befabd76979497629
JerryWski <jarek_wasilewski@yahoo.com>
2022-10-29 21:09:29 +0200

    js

--- javascript/main.js
+++ javascript/main.js
@@ -1,51 +1,48 @@
-const username = document.querySelector('#name');
-const surname = document.querySelector('#surname');
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
-const submitBtn = document.querySelector('.submit-btn');
-
-
+const username = document.querySelector("#name");
+const surname = document.querySelector("#surname");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const submitBtn = document.querySelector(".submit-btn");
+const textError = document.querySelector('.text-error')
+console.log(textError);
 
 const showError = (input) => {
-    const formBox = input.parentElement;
-    formBox.classList.add('error');
-}
+  const formBox = input.parentElement;
+  formBox.classList.add("error");
+};
 
 const removeError = (input) => {
-    const formBox = input.parentElement;
-    formBox.classList.remove('error');
-}
-
+  const formBox = input.parentElement;
+  formBox.classList.remove("error");
+};
 
 const checkForm = (input) => {
-  input.forEach(el => {
-    if(el.value === ''){
-        showError(el)
-    }else {
-     removeError(el)   
+  input.forEach((el) => {
+    if (el.value === "") {
+      showError(el);
+    } else {
+      removeError(el);
     }
-  })  
+  });
+};
 
-
-    
+const checkMail= (email) => {
+ const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+ if(re.test(email.value)) {
+    removeError(email)
+ }else {
+    showError(email)
+ }
 }
 
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const inputs = [username, surname, email, password];
 
+  checkForm(inputs);
+  checkMail(email)
 
-submitBtn.addEventListener('click', e => {
-e.preventDefault();
-const inputs =[username, surname,email,password];
-
-checkForm(inputs)
-
-
-//clearing
-
-// inputs.forEach(el => {
-//     el.value='';
-// })
-}
-)
-
-
-
+//   inputs.forEach((el) => {
+//     el.value = "";
+//   });
+});
